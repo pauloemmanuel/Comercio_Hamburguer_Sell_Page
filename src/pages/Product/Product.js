0939c9b13618a3250f197c47495782e4ded4c3aa
Paulo Emmanuel <pauloemmanuelferreira@gmail.com>
2022-03-01 12:49:37 -0300

    Pagina produtos Desenvolvida

--- src/pages/Product/Product.js
+++ src/pages/Product/Product.js
@@ -1,8 +1,23 @@
-import {PageContainer,Inline} from '../../components/MainComponents'
-import {Options__Children,Options__Header,HamburgerContainer,HamburgerContainer__Image,Options,ContainerInline,HamburgerContainer__ProductTitle,HamburgerContainer__ProductDescription,HamburgerContainer__ProductPrice,HamburgerContainer__OldPrice} from './ProductStyle'
-const Product = () =>{
-  
-  return(
+import { PageContainer, Inline } from '../../components/MainComponents'
+import { useState } from 'react';
+import { Options__Divider,Options__Children__Counter__Big, Options__Buy, Options__Children__Counter, Options__Children, Options__Header, HamburgerContainer, HamburgerContainer__Image, Options, ContainerInline, HamburgerContainer__ProductTitle, HamburgerContainer__ProductDescription, HamburgerContainer__ProductPrice, HamburgerContainer__OldPrice } from './ProductStyle'
+const Product = () => {
+
+  const [contador, setContador] = useState(0);
+
+  const handleNumberMinus = () => {
+    if (contador <= 0) {
+      setContador(0);
+      window.alert('Não pode ser abaixo de 0 😡')
+    } else {
+      setContador(contador - 1);
+    }
+  }
+  const handleNumberPlus = () => {
+    setContador(contador + 1);
+  }
+
+  return (
     <PageContainer>
       <HamburgerContainer>
         <HamburgerContainer__Image>
@@ -14,24 +29,89 @@ const Product = () =>{
         <HamburgerContainer__ProductDescription>
           Hambúrguer de picanha, molho de picanha, cebola crispy, bacon, queijo cheddar, molho cheddar e pão mix de gergelim
         </HamburgerContainer__ProductDescription>
-         <ContainerInline>
-         <HamburgerContainer__ProductPrice>
-           R$31,99
+        <ContainerInline>
+          <HamburgerContainer__ProductPrice>
+            R$31,99
           </HamburgerContainer__ProductPrice>
           <HamburgerContainer__OldPrice>
-           R$34,95
-         </HamburgerContainer__OldPrice>
-         </ContainerInline>
+            R$34,95
+          </HamburgerContainer__OldPrice>
+        </ContainerInline>
       </HamburgerContainer>
       <Options>
-        <Options__Header>
-          <p>Adicionar Ingredientes</p>
-          <span>Até 8 ingredientes</span>
-        </Options__Header>
-        <Options__Children>
-          Queijo Cheddar
-        </Options__Children>
+        <Options__Divider>
+          <Options__Header>
+            <p>Adicionar Ingredientes</p>
+            <span>Até 8 ingredientes</span>
+          </Options__Header>
+          <Options__Children>
+            <span>Queijo Cheddar</span>
+            <Options__Children__Counter>
+              <div className="input-number">
+                <button type="button" onClick={handleNumberMinus} >&minus;</button>
+                <span>{contador}</span>
+                <button type="button" onClick={handleNumberPlus}>&#43;</button>
+              </div>
+            </Options__Children__Counter>
+            <span className='orangeText'>+ R$4,99</span>
+          </Options__Children>
+          <Options__Children>
+            <span>Queijo Cheddar</span>
+            <Options__Children__Counter>
+              <div className="input-number">
+                <button type="button" onClick={handleNumberMinus} >&minus;</button>
+                <span>{contador}</span>
+                <button type="button" onClick={handleNumberPlus}>&#43;</button>
+              </div>
+            </Options__Children__Counter>
+            <span className='orangeText'>+ R$4,99</span>
+          </Options__Children>
+          <Options__Children>
+            <span>Queijo Cheddar</span>
+            <Options__Children__Counter>
+              <div className="input-number">
+                <button type="button" onClick={handleNumberMinus} >&minus;</button>
+                <span>{contador}</span>
+                <button type="button" onClick={handleNumberPlus}>&#43;</button>
+              </div>
+            </Options__Children__Counter>
+            <span className='orangeText'>+ R$4,99</span>
+          </Options__Children>
+          <Options__Children>
+            <span>Queijo Cheddar</span>
+            <Options__Children__Counter>
+              <div className="input-number">
+                <button type="button" onClick={handleNumberMinus} >&minus;</button>
+                <span>{contador}</span>
+                <button type="button" onClick={handleNumberPlus}>&#43;</button>
+              </div>
+            </Options__Children__Counter>
+            <span className='orangeText'>+ R$4,99</span>
+          </Options__Children>
+          <Options__Header>
+            <p>Precisa de Talher?</p>
+          </Options__Header>
+          <Options__Children>
+            <label><span>Sim</span> <input type='radio' name='talheryesorno'/></label> 
+            <br/><br/>
+            <label><span>Não</span> <input type='radio' name='talheryesorno'/></label>
+          </Options__Children>
+        </Options__Divider>
+        <Options__Divider>
+          <Options__Buy>
+            <Options__Children__Counter__Big>
+              <div className="input-number">
+                <button type="button" onClick={handleNumberMinus} >&minus;</button>
+                <span>{contador}</span>
+                <button type="button" onClick={handleNumberPlus}>&#43;</button>
+              </div>
+            </Options__Children__Counter__Big>
+            <button className='addButton'>Adicionar</button>
+          </Options__Buy>
+        </Options__Divider>
+
       </Options>
+
     </PageContainer>
   );
 }
