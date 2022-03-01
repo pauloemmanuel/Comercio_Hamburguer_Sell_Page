0b758e3b89e756a9664283e08ba53bc261dd2b83
Paulo Emmanuel <pauloemmanuelferreira@gmail.com>
2022-03-01 12:49:19 -0300

    Funcionalidade de Endereço
    Agora o endereço pode ser trocado

--- src/components/partials/Header/Header.js
+++ src/components/partials/Header/Header.js
@@ -4,17 +4,24 @@ import { Link } from 'react-router-dom';
 import { useState } from 'react';
 const Header = () => {
   const [carrinho,setCarrinho] = useState(0);
-
+  const [enderecos,setEnderecos] = useState(['R. Antonio Braune, 222','Rua Jorge Amado,821','Avenida Freitas Silvas,981','R. Maria Bernades, 819'])
+  const [enderecoAtual,setEnderecoAtual] = useState(enderecos[0]);
   const exibirEnderecos = () => {
    let el = document.querySelector('#enderecos');
-
    if(el.style.display == 'block'){
-     console.log('a'+el.style.display)
      el.style.display = 'none'
    }else{
-    console.log('b'+el.style.display)
     el.style.display = 'block'
    }
+  }
+  function mudarEnderecos(index){
+    let novosEnderecos = enderecos;
+    let respiro = novosEnderecos[0];
+    novosEnderecos[0] = novosEnderecos[index];
+    novosEnderecos[index] = respiro;
+    setEnderecos(novosEnderecos);
+    setEnderecoAtual(novosEnderecos[0])
+    console.log(enderecos)
   }
 
   return(
@@ -25,15 +32,20 @@ const Header = () => {
           <Link to='/'><img src='./img/DeliverizeLogo.png'></img></Link>
         </HeaderBox__Logo>
         <HeaderBox__Container>
-          <HeaderBox__SelectAddress>  
-            <span><span class="orangeText">Entrega:</span> <br/>
-            R. Antonio Braune, 222
+          <HeaderBox__SelectAddress  onClick={exibirEnderecos}>  
+            <span><span className="orangeText">Entrega:</span> <br/>
+            {enderecoAtual}
             </span>
-           <img onClick={exibirEnderecos} src='./img/SetaParaBaixoLaranja.svg'></img>
+           <img src='./img/SetaParaBaixoLaranja.svg'></img>
            <ul id='enderecos'>
-            <li>Endereço 2</li>
-            <li>Endereço 3</li>
-            <li>Endereço 4</li>
+           {enderecos.map((item,index)=>{
+             if(index != 0){
+              return(
+                <li onClick={()=>{mudarEnderecos(index)}}>{item}</li>
+              );}
+           })}
+            
+           
            </ul>
           </HeaderBox__SelectAddress>
           <HeaderBox__SearchInput>
@@ -47,6 +59,9 @@ const Header = () => {
             <Header__Counter>{carrinho}</Header__Counter>
             <img src='./img/CarrinhoCompras.svg'></img>
             <p>Carrinho</p>
+            <div>
+              
+            </div>
           </HeaderBox__ImgPlusText>
         </HeaderBox__Container>
       </PageContainer>
